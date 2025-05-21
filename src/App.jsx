329fdc62ba1965bcdbf78fdbb99b781ac6283018
Rajat Srivastav <ratneshrsrivastav@gmail.com>
2025-05-22 01:05:07 +0530

    Folder Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import { Navbar } from './components/Navbar'
+import { Overview } from './components/Overview'
+import { Setup } from './components/Setup'
+import { Controls } from './components/controls'
+import { Safety } from './components/safety'
+import {Footer} from './components/Footer'
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar/>
+      <main>
+        <Overview/>
+        <Setup/>
+        <Controls/>
+        <Safety/>
+      </main>
+      <Footer/>
+    </div>
   )
 }
 
---
+++ src/components/Controls.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Controls = () => {
+  return (
+    <div>C</div>
+  )
+}
